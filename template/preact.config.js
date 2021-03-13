f4b57970e5c7e7019e54a25a69a030c152812fee
Michael Rüttgers <michael@ruettgers.eu>
2021-03-13 23:09:21 +0100

    Removed debug output

--- template/preact.config.js
+++ template/preact.config.js
@@ -4,7 +4,6 @@ const ESPBuildPlugin = require('./esp/build');
 export default {
     webpack(config, env, helpers, options) {
         if (env.isProd) {
-            console.log(env);
             config.devtool = false;
             config.plugins = [
                 ...config.plugins,
